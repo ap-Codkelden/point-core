304169afba1456852ba0eede4ce5e23964463cea
ap-Codkelden <mavladi@gmail.com>
2015-06-25 23:39:06 +0300

    redefined makeFootnotesDiv added as RemoveHRFromFootnotesDiv

--- lib/point/util/md.py
+++ lib/point/util/md.py
@@ -1,6 +1,9 @@
+# -*- coding: UTF-8 -*-
+
 from markdown.preprocessors import Preprocessor
 from markdown.inlinepatterns import Pattern, LinkPattern
 from markdown.util import etree
+
 try:  
     from urllib.parse import urlparse, urlunparse
 except ImportError:
@@ -130,3 +133,43 @@ class ColonLinkPattern(LinkPattern):
 
         # Url passes all tests. Return url as-is.
         return urlunparse(url)
+
+
+def RemoveHRFromFootnotesDiv(self, root):
+    """Возвращает блок сносок <div> как элемент ElementTree. 
+    Отличается от нативного метода класса FootnoteExtension только 
+    ампутированным <hr> (ниже он закомментирован)"""
+
+    if not list(self.footnotes.keys()):
+        return None
+
+    div = etree.Element("div")
+    div.set('class', 'footnote')
+    # etree.SubElement(div, "hr")
+    ol = etree.SubElement(div, "ol")
+
+    for id in self.footnotes.keys():
+        li = etree.SubElement(ol, "li")
+        li.set("id", self.makeFootnoteId(id))
+        self.parser.parseChunk(li, self.footnotes[id])
+        backlink = etree.Element("a")
+        backlink.set("href", "#" + self.makeFootnoteRefId(id))
+        if self.md.output_format not in ['html5', 'xhtml5']:
+            backlink.set("rev", "footnote")  # Invalid in HTML5
+        backlink.set("class", "footnote-backref")
+        backlink.set(
+            "title",
+            "Jump back to footnote %d in the text" %
+            (self.footnotes.index(id)+1)
+        )
+        backlink.text = FN_BACKLINK_TEXT
+
+        if li.getchildren():
+            node = li[-1]
+            if node.tag == "p":
+                node.text = node.text + NBSP_PLACEHOLDER
+                node.append(backlink)
+            else:
+                p = etree.SubElement(li, "p")
+                p.append(backlink)
+    return div
