b939e232c7211d7e61b2ecb050018246cd295277
ap-Codkelden <mavladi@gmail.com>
2015-06-28 22:21:58 +0300

    deviantart helper

---
+++ lib/point/util/deviantart.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+"""
+Получение и обработка данных об изображении DeviantArt при помощи oEmbed
+========================================================================
+
+(c) Point.im Project, 2015
+
+Документация DeviantArt oEmbed <https://www.deviantart.com/developers/oembed>
+"""
+
+import urllib2
+import json
+#from markdown.util import etree
+
+
+class DeviantArtPreview:
+    def __init__(self):
+        self.DEVIANT_OEMBED_URL = "http://backend.deviantart.com/oembed?url="
+
+    def deviant_preview(self, url):
+        try:
+            response = urllib2.urlopen('%s%s' % (self.DEVIANT_OEMBED_URL, url))
+            s = response.read()
+        except Exception:
+            wrap = etree.Element('div')
+            a = etree.SubElement(wrap, 'a')
+            a.set('href', url)
+            return url 
+        else:
+            data = json.loads(s)
+            wrap = etree.Element('div')
+            wrap.set('class', 'clearfix')
+            a = etree.SubElement(wrap, 'a')
+            a.set('href', url)
+            a.set('class', 'postimg')
+            img = etree.SubElement(a, 'img')
+            img.set('src', data['thumbnail_url'])
+            img.set('alt', data['title']+" by "+data['author_name'])
+            img.set('height', data['thumbnail_height_200h'])
+
+            return wrap
+
+
+if __name__ == "__main__":
+    d = DeviantArtPreview()
+    url = ('http://jasonchanart.deviantart.com/art/Maul-Splicer-542484356')
+    d.deviant_preview(url)
